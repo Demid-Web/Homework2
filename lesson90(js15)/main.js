83d892afbe6089702d7b37c797d966e6d7837e8b
Demid-Web <mahno.2004@mail.ru>
2024-08-20 11:11:10 +0300

    Add files via upload

---
+++ lesson90(js15)/main.js
@@ -0,0 +1,13 @@
+/*const btns = document.querySelectorAll('.btn')
+
+btns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        console.log(index+1)
+    })
+})*/
+
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click', (event) => {
+    event.target.classList.toggle('red')
+})
