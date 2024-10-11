7da7379c56810422f545b200557342780bdc7acc
Demid-Web <mahno.2004@mail.ru>
2024-10-11 21:24:38 +0300

    Add files via upload

--- SOS/js/main.js
+++ SOS/js/main.js
@@ -7,3 +7,11 @@ const swiper = new Swiper('.swiper', {
         disableOnInteraction: false,
     },
 });
+const burgerControll = document.querySelector('.burger__control')
+const burger = document.querySelector('.burger__menu')
+const body = document.querySelector('.body')
+burgerControll.addEventListener('click', () => {
+    burger.classList.toggle('burger__menu--active')
+    body.classList.toggle('body--dont-scrolling')
+    burgerControll.classList.toggle('burger__control--active')
+})
