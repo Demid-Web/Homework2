fa1b7c51f580e631a16c3825032c034bd8dd002a
Demid-Web <mahno.2004@mail.ru>
2024-10-13 17:44:57 +0300

    Add files via upload

--- SOS/js/main.js
+++ SOS/js/main.js
@@ -1,10 +1,18 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.header__swiper', {
 
     slidesPerView: 1,
     loop: true,
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
 });
 const burgerCell = document.querySelector('.burger__cell')
@@ -16,3 +24,16 @@ burgerCell.addEventListener('click', () => {
     body.classList.toggle('body--dont-scrolling')
     burgerControll.classList.toggle('burger__control--active')
 })
+const swiperAbout = new Swiper(".about-swiper", {
+    effect: "cards",
+    grabCursor: true,
+
+    pagination: {
+        el: ".swiper-pagination-about",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next-about",
+        prevEl: ".swiper-button-prev-about",
+    },
+});
