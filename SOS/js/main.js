847683d485b6b4d596777465af440e8df77d6090
Demid-Web <mahno.2004@mail.ru>
2024-10-20 16:02:26 +0300

    Add files via upload

--- SOS/js/main.js
+++ SOS/js/main.js
@@ -37,3 +37,24 @@ const swiperAbout = new Swiper(".about-swiper", {
         prevEl: ".swiper-button-prev-about",
     },
 });
+
+const accordion = document.querySelector('.accordion')
+    accordion.addEventListener('click', accordionContentShow)
+function accordionContentShow(event) {
+    const accordionControl = event.target.closest('.accordion__cell')
+    const accordionButton = document.querySelectorAll('.accordion__button__link')
+    for (i = 0; i < accordionButton.length; i++) {
+        console.log(accordionButton[i])
+        /*accordionButton[i].preventDefault()*/
+    }
+    const accordionContent = accordionControl.children[1]
+    console.log(accordionContent)
+    if (!accordionControl) return
+    accordionControl.classList.toggle('accordion__cell--focus')
+    accordionContent.classList.toggle('accordion__content--active')
+    if (accordionContent.classList.contains('accordion__content--active')) {
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'
+    } else {
+        accordionContent.style.maxHeight = null
+    }
+}
